fde01d90afdaa675005262da7c6831ab66fa2580
sshaw <skye.shaw@gmail.com>
2014-05-13 21:56:45 -0400

    update $VERSION

--- lib/Mojolicious/Plugin/DigestAuth.pm
+++ lib/Mojolicious/Plugin/DigestAuth.pm
@@ -10,7 +10,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojolicious::Plugin::DigestAuth::DB;
 use Mojolicious::Plugin::DigestAuth::RequestHandler;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 sub register
 {
